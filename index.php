54f801e1e49bcb122e1c884968a43e30bf4a13f5
ghelpo <m.f.ragab5890@gmail.com>
2020-09-29 22:45:43 +0200

    edit-sources

--- index.php
+++ index.php
@@ -12,32 +12,11 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Metal+Mania&display=swap" rel="stylesheet">
-  <title>Manipulating the DOM</title>
+  <title>Cairo Events</title>
   <!-- Load Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Fira+Sans:900|Merriweather&display=swap" rel="stylesheet">  <!-- Load Styles -->
   <link href="css/styles.css" rel="stylesheet">
 </head>
-	<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.35">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Metal+Mania&display=swap" rel="stylesheet">
-		<script type="text/javascript">
-
-			$(window).load(function() {
-			// Animate loader off screen
-			$(".loading").fadeOut("slow");;
-			});
-
-		</script>
-
-		<title>Cairo-Events</title>
-
-
-
-
-
-	</head>
 
 	<body>
 
@@ -279,24 +258,28 @@
 			</div>
 
 
+      <script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+      <script type="text/javascript">
 
+        $(window).load(function() {
+        // Animate loader off screen
+        $(".loading").fadeOut("slow");;
+        });
+
+    		function check()
+    		{
+    			document.getElementById("agree").checked = true;
+    		}
+
+
+    	</script>
+      <script src="js/app.js" charset="utf-8"></script>
 	</body>
-  <script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
-  <script src="main.js" async></script>
-	<script type="text/javascript">
-		function check()
-		{
-			document.getElementById("agree").checked = true;
-		}
-
-
-	</script>
-
 
 
 
