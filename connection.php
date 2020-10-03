76e80a4f646e5873d9a1c84e9ff2c2ab3a297d1a
ghelpo <m.f.ragab5890@gmail.com>
2020-10-04 00:28:25 +0200

    style
    update page style

--- connection.php
+++ connection.php
@@ -1,6 +1,16 @@
+
 <?php
 
-    $link = mysqli_connect("localhost", "user", "password", "cairo-events");
+$user='ghelpo';
+$pass='tafiTAFI';
+$databasename='ghelpo';
+
+date_default_timezone_set("Africa/Cairo");
+
+
+
+
+   $link = mysqli_connect("localhost", $user, $pass, $databasename);
 
         if (mysqli_connect_error()) {
 
